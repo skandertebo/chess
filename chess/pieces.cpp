0e34b6ef2bf48b9288abf91b55413aeb31ecf9f8
skandertebo <alexandertebourb@gmail.com>
2022-01-20 20:24:03 +0100

    state 1

--- chess/pieces.cpp
+++ chess/pieces.cpp
@@ -1,22 +1,22 @@
 #include "pieces.h"
 namespace piece {
 bool rook :: valid_move(int newline , char newcolumn , piece***board){
-
+	return 0;
 }
 bool pawn :: valid_move(int newline , char newcolumn , piece***board){
-
+	return 0;
 }
 bool bishop :: valid_move(int newline , char newcolumn , piece***board){
-
+	return 0;
 }
 bool queen :: valid_move(int newline , char newcolumn , piece***board){
-
+	return 0;
 }
 bool king :: valid_move(int newline , char newcolumn , piece***board){
-
+	return 0;
 }
 bool knight :: valid_move(int newline , char newcolumn , piece***board){
-
+	return 0;
 }
 
 void promote(piece* pwn , char prom){
