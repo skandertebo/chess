ee5a48989aefad3fb1523d9dd24b8e83e344961f
skandertebo <alexandertebourb@gmail.com>
2022-01-20 23:04:16 +0100

    in work

--- chess/pieces.cpp
+++ chess/pieces.cpp
@@ -1,25 +1,39 @@
 #include "pieces.h"
 namespace piece {
-bool rook :: valid_move(int newline , char newcolumn , piece***board){
+bool rook :: valid_move(int newline , int newcolumn , piece***board){
+	int i;
+	if ((newline > 7) || (newcolumn) > 7) return false;
+	if ((line != newline) && (column != newcolumn)) return false;
+	if ((line == newline) && (column == column)) return false;
+	if ((board[newline][newcolumn] != nullptr) && (board[newline][newcolumn]->getcolor() == color))return false;
+	if (newline > line) {
+		for (i = line + 1; i < newline; i++) {
+			if (board[i][column] != nullptr) return false;
+		}
+		
+
+
+
+	}
+}
+bool pawn :: valid_move(int newline , int newcolumn , piece***board){
 	return 0;
 }
-bool pawn :: valid_move(int newline , char newcolumn , piece***board){
+bool bishop :: valid_move(int newline , int newcolumn , piece***board){
 	return 0;
 }
-bool bishop :: valid_move(int newline , char newcolumn , piece***board){
+bool queen :: valid_move(int newline , int newcolumn , piece***board){
 	return 0;
 }
-bool queen :: valid_move(int newline , char newcolumn , piece***board){
+bool king :: valid_move(int newline , int newcolumn , piece***board){
 	return 0;
 }
-bool king :: valid_move(int newline , char newcolumn , piece***board){
-	return 0;
-}
-bool knight :: valid_move(int newline , char newcolumn , piece***board){
+bool knight :: valid_move(int newline , int newcolumn , piece***board){
 	return 0;
 }
 
 void promote(piece* pwn , char prom){
+	if(pwn->prom == 'p'){
 	switch (prom) {
 	case 'r':
 	{rook extra(pwn->getcolor(), pwn->getline(), pwn->getcolumn(), pwn->getstatus()); 
@@ -49,7 +63,7 @@ void promote(piece* pwn , char prom){
 	};
 	break;
 	}
- }
-
+    }
+	
 
  };
--- chess/pieces.h
+++ chess/pieces.h
@@ -27,21 +27,22 @@ char column ;
 int color ;
 
 public : 
-
-virtual bool valid_move(int newline , char newcolumn , piece*** board)=0;
-void move(piece& p , int newline , int newcolumn);
-piece(int color , int line , char column , bool status):color(color) , status(status) , line(line) , column(column) {};
+static const char prom;
+virtual bool valid_move(int newline , int newcolumn , piece*** board)=0; 
+// note that valid move only checks the piece's move validity without looking into exposing the king to a check 
+// for the piece to have an actual valid move it should pass the valid_move method and the check situation function
+piece(int color , int line , int column , bool status):color(color) , status(status) , line(line) , column(column) {};
 ~piece(){}
 piece(const piece& pce) : color(pce.color) , line(pce.line) , column(pce.column) , status(pce.status) {};
-void move(int line , int column , piece***board);
-void kill(bool status , int line , int column);
+void move(int newline , int newcolumn , piece***board);
+void kill();
 bool getstatus(){return status;}
 int  getcolor(){return color;}
 int getline(){return line;}
-char getcolumn(){return column;}
+int getcolumn(){return column;}
 void setcolor(int color){this->color = color;}
 void setline(int line){this->line = line;}
-void setcolumn(char column){this->column = column;}
+void setcolumn(int column){this->column = column;}
 void setstatus(bool status){this->status = status;}
 
 
@@ -49,48 +50,48 @@ void setstatus(bool status){this->status = status;}
 
 class pawn : public piece{
 public:
-bool valid_move(int newline , char newcolumn , piece*** board);
-//implementing upgrade to queen should be member or friend function ?
-pawn(int color, int line, char column, bool status) : piece(color, line, column, status) {};
+bool valid_move(int newline , int newcolumn , piece*** board);
+pawn(int color, int line, int column, bool status) : piece(color, line, column, status) {};
 pawn(const pawn& pwn) : piece(pwn){};
+static const char prom = 'p';
 };
 
 class rook : public piece{
 public:
-bool valid_move(int newline , char newcolumn , piece*** board);
-rook(int color , int line , char column , bool status) : piece(color , line , column , status){};
+bool valid_move(int newline , int newcolumn , piece*** board);
+rook(int color , int line , int column , bool status) : piece(color , line , column , status){};
 static const char prom = 'r' ;
 rook(const rook& rk) : piece(rk){};
 };
 
 class queen : public piece{
 public:
-bool valid_move(int newline , char newcolumn , piece*** board);
-queen(int color , int line , char column , bool status) : piece(color , line , column , status){};
+bool valid_move(int newline , int newcolumn , piece*** board);
+queen(int color , int line , int column , bool status) : piece(color , line , column , status){};
 static const char prom = 'q' ;
 queen(const queen& q) : piece(q) {};
 };
 
 class knight : public piece{
 public:
-bool valid_move(int newline , char newcolumn , piece*** board);
-knight(int color , int line , char column , bool status) : piece(color , line , column , status){};
+bool valid_move(int newline , int newcolumn , piece*** board);
+knight(int color , int line , int column , bool status) : piece(color , line , column , status){};
 static const char prom = 'k' ;
 knight(const knight& k) : piece(k) {};
 };
 
 class bishop : public piece{
 public:
-bool valid_move(int newline , char newcolumn , piece*** board);
-bishop(int color , int line , char column , bool status) : piece(color , line , column , status){};
+bool valid_move(int newline , int newcolumn , piece*** board);
+bishop(int color , int line , int column , bool status) : piece(color , line , column , status){};
 static const char prom = 'b' ;
 bishop(const rook& bi) : piece(bi) {};
 };
 
 class king : public piece{
 public:
-bool valid_move(int newline , char newcolumn , piece*** board);
-king(int color , int line , char column , bool status) : piece(color , line , column , status){};
+bool valid_move(int newline , int newcolumn , piece*** board);
+king(int color , int line , int column , bool status) : piece(color , line , column , status){};
 bool in_check();
 bool in_check_mate();
 };
