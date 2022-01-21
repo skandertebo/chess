d36f0fa46cb57835c4891815defb4c39cf319ede
skandertebo <alexandertebourb@gmail.com>
2022-01-21 02:58:38 +0100

    in work

--- chess/pieces.cpp
+++ chess/pieces.cpp
@@ -1,9 +1,9 @@
 #include "pieces.h"
 namespace piece {
+	
 bool rook :: valid_move(int newline , int newcolumn , piece***board){
 	int i;
 	piece* temp = this ;
-	piece* alt;
 	if ((newline > 7) || (newcolumn) > 7) return false;
 	if ((line != newline) && (column != newcolumn)) return false;
 	if ((line == newline) && (column == column)) return false;
@@ -30,16 +30,31 @@ bool rook :: valid_move(int newline , int newcolumn , piece***board){
 		}
 
 	}
-
 	
-
-	
+	return true;
 }
 bool pawn :: valid_move(int newline , int newcolumn , piece***board){
-	return 0;
+	if ((newline > 7) || (newcolumn) > 7) return false;
+	if (newline <= line) return false;
+	if ((newline > line + 2)||(newcolumn > column + 1)||(newcolumn < column - 1)) return false;
+	if ((board[newline][newcolumn] != nullptr) && (board[newline][newcolumn]->getcolor() == color))return false;
+	if (newline == line + 1) {
+	if (((newcolumn == column - 1) || (newcolumn == column + 1)) && (board[newline][newcolumn] == nullptr)) return false;
+	if ((newcolumn == column) && (board[newline][newcolumn] != nullptr))return false;
+	return true;
+	}
+	else {
+		
+			if (newcolumn != column)return false;
+			if (board[line + 1][column] != nullptr)return false;
+			if (board[newline][newcolumn] != nullptr) return false;
+
+			return true;
+	}
 }
 bool bishop :: valid_move(int newline , int newcolumn , piece***board){
-	return 0;
+	if ((newline > 7) || (newcolumn) > 7) return false;
+	if ((board[newline][newcolumn] != nullptr) && (board[newline][newcolumn]->getcolor() == color))return false;
 }
 bool queen :: valid_move(int newline , int newcolumn , piece***board){
 	return 0;
@@ -50,39 +65,40 @@ bool king :: valid_move(int newline , int newcolumn , piece***board){
 bool knight :: valid_move(int newline , int newcolumn , piece***board){
 	return 0;
 }
-
+bool in_check(const king& kng) { return 0; }
 void promote(piece* pwn, char prom) {
-	if (pwn->prom == 'p') {
-		switch (prom) {
-		case 'r':
-		{rook extra(pwn->getcolor(), pwn->getline(), pwn->getcolumn(), pwn->getstatus());
-		delete pwn;
-		*pwn = rook(extra);
-		};
-		break;
+	
+		if (pwn->getprom() == 'p') {
+			switch (prom) {
+			case 'r':
+			{rook extra(pwn->getcolor(), pwn->getline(), pwn->getcolumn(), pwn->getstatus() , pwn->getmove_count());
+			delete pwn;
+			*pwn = rook(extra);
+			};
+			break;
 
-		case 'k':
-		{knight extra(pwn->getcolor(), pwn->getline(), pwn->getcolumn(), pwn->getstatus());
-		delete pwn;
-		*pwn = knight(extra);
-		};
-		break;
+			case 'k':
+			{knight extra(pwn->getcolor(), pwn->getline(), pwn->getcolumn(), pwn->getstatus(), pwn->getmove_count());
+			delete pwn;
+			*pwn = knight(extra);
+			};
+			break;
 
-		case 'q':
-		{queen extra(pwn->getcolor(), pwn->getline(), pwn->getcolumn(), pwn->getstatus());
-		delete pwn;
-		*pwn = queen(extra);
-		};
-		break;
+			case 'q':
+			{queen extra(pwn->getcolor(), pwn->getline(), pwn->getcolumn(), pwn->getstatus(), pwn->getmove_count());
+			delete pwn;
+			*pwn = queen(extra);
+			};
+			break;
 
-		case 'b':
-		{bishop extra(pwn->getcolor(), pwn->getline(), pwn->getcolumn(), pwn->getstatus());
-		delete pwn;
-		*pwn = bishop(extra);
-		};
-		break;
+			case 'b':
+			{bishop extra(pwn->getcolor(), pwn->getline(), pwn->getcolumn(), pwn->getstatus(), pwn->getmove_count());
+			delete pwn;
+			*pwn = bishop(extra);
+			};
+			break;
+			}
 		}
 	}
-}
-
+ 
  };
--- chess/pieces.h
+++ chess/pieces.h
@@ -22,77 +22,77 @@ class piece {
 protected : 
 
 bool status;
+int move_count;
 int line ;
 char column ;
 int color ;
-
+char prom; //to identify the piece type when promoting a pawn
 public : 
-static const char prom;
+char getprom() { return prom; }
 virtual bool valid_move(int newline , int newcolumn , piece*** board)=0; 
 // note that valid move only checks the piece's move validity without looking into exposing the king to a check 
 // for the piece to have an actual valid move it should pass the valid_move method and the check situation function
-piece(int color , int line , int column , bool status):color(color) , status(status) , line(line) , column(column) {};
+piece(int color , int line , int column , bool status , int move_count ):color(color) , status(status) , line(line) , column(column) , move_count(move_count) {};
 ~piece(){}
-piece(const piece& pce) : color(pce.color) , line(pce.line) , column(pce.column) , status(pce.status) {};
+piece(const piece& pce) : color(pce.color) , line(pce.line) , column(pce.column) , status(pce.status) , prom(pce.prom) , move_count(pce.move_count) {};
 void move(int newline , int newcolumn , piece***board);
 void kill();
 bool getstatus(){return status;}
 int  getcolor(){return color;}
 int getline(){return line;}
 int getcolumn(){return column;}
+int getmove_count() { return move_count; }
 void setcolor(int color){this->color = color;}
 void setline(int line){this->line = line;}
 void setcolumn(int column){this->column = column;}
 void setstatus(bool status){this->status = status;}
-
+//implement discovery check move validity method
+//idea is to make the move and check move validity on all enemy pieces
 
 };
 
 class pawn : public piece{
 public:
 bool valid_move(int newline , int newcolumn , piece*** board);
-pawn(int color, int line, int column, bool status) : piece(color, line, column, status) {};
+pawn(int color, int line, int column, bool status , int move_count) : piece(color, line, column, status, move_count ) { prom = 'p'; };
 pawn(const pawn& pwn) : piece(pwn){};
-static const char prom = 'p';
+
 };
 
 class rook : public piece{
 public:
 bool valid_move(int newline , int newcolumn , piece*** board);
-rook(int color , int line , int column , bool status) : piece(color , line , column , status){};
-static const char prom = 'r' ;
+rook(int color , int line , int column , bool status, int move_count) : piece(color , line , column , status , move_count){ prom = 'r'; };
+
 rook(const rook& rk) : piece(rk){};
 };
 
 class queen : public piece{
 public:
 bool valid_move(int newline , int newcolumn , piece*** board);
-queen(int color , int line , int column , bool status) : piece(color , line , column , status){};
-static const char prom = 'q' ;
+queen(int color , int line , int column , bool status, int move_count) : piece(color , line , column , status, move_count){ prom = 'q'; };
 queen(const queen& q) : piece(q) {};
 };
 
 class knight : public piece{
 public:
 bool valid_move(int newline , int newcolumn , piece*** board);
-knight(int color , int line , int column , bool status) : piece(color , line , column , status){};
-static const char prom = 'k' ;
+knight(int color , int line , int column , bool status, int move_count) : piece(color , line , column , status, move_count){ prom = 'k'; };
 knight(const knight& k) : piece(k) {};
 };
 
 class bishop : public piece{
 public:
 bool valid_move(int newline , int newcolumn , piece*** board);
-bishop(int color , int line , int column , bool status) : piece(color , line , column , status){};
-static const char prom = 'b' ;
+bishop(int color , int line , int column , bool status, int move_count) : piece(color , line , column , status, move_count){ prom = 'b'; };
 bishop(const rook& bi) : piece(bi) {};
 };
 
 class king : public piece{
 public:
 bool valid_move(int newline , int newcolumn , piece*** board);
-king(int color , int line , int column , bool status) : piece(color , line , column , status){};
-bool in_check();
+king(int color , int line , int column , bool status, int move_count) : piece(color , line , column , status, move_count){};
+bool in_check(const king& kng);
 bool in_check_mate();
 };
 void promote(piece* pwn , char prom );
